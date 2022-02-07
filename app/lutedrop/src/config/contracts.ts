ee3d9c6d39f2d9bb1e8d677f7e74e39d8752d9f7
Connor Mendenhall <cmendenhall@8thlight.com>
2022-02-06 19:54:16 -0500

    Add Mumbai contracts

--- app/lutedrop/src/config/contracts.ts
+++ app/lutedrop/src/config/contracts.ts
@@ -49,6 +49,100 @@ const config = {
       ]),
     },
   },
+  [ChainId.Mumbai]: {
+    lute: {
+      address: "0xF782D55BBef8028f965Bf542d8FBf3993aF9B800",
+      abi: new Interface([
+        "function totalSupply() external view returns (uint256)",
+        "function tokenURI(uint256 tokenId) external view returns (string memory)",
+        "function setApprovalForAll(address operator, bool approved) external",
+        "function isApprovedForAll(address owner, address operator) external view returns (bool)",
+        "event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId)",
+        "event ApprovalForAll(address indexed owner, address indexed operator, bool approved)",
+        "event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)",
+      ]),
+    },
+    flute: {
+      address: "0x1ae67219b798Db3A7fc03D5cfaAc583CcB55C295",
+      abi: new Interface([
+        "function totalSupply() external view returns (uint256)",
+        "function tokenURI(uint256 tokenId) external view returns (string memory)",
+        "function setApprovalForAll(address operator, bool approved) external",
+        "function isApprovedForAll(address owner, address operator) external view returns (bool)",
+        "event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId)",
+        "event ApprovalForAll(address indexed owner, address indexed operator, bool approved)",
+        "event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)",
+      ]),
+    },
+    luteDrop: {
+      address: "0xA78a53b92aAb1eF0EFcdBA0f654B4087185F2Be0",
+      abi: new Interface([
+        "function latestDrop() external view returns (uint256)",
+        "function drops(uint256 dropId) external view returns (tuple(uint256 fee, uint256 craftableSupply, uint256 craftedSupply, uint256 craftsPerAddress))",
+        "function crafts(address account, uint256 dropid) external view returns (uint256)",
+        "function craft(uint8 item, uint256 dropid) external payable",
+        "event Craft(address indexed to, uint8 indexed item, uint256 fee)",
+      ]),
+    },
+    lutiswap: {
+      address: "0xc4e008F5bDD5e2c138A6a7E827076d45450D430E",
+      abi: new Interface([
+        "function nextLute() external view returns (uint256)",
+        "function nextFlute() external view returns (uint256)",
+        "function latestSwapPrice() external view returns (uint256, uint256)",
+        "function swapExactLuteForFlute(uint256 tokenId) external payable",
+        "function swapExactFluteForLute(uint256 tokenId) external payable",
+        "event Swap(address indexed user, address indexed from, address indexed to, uint256 fromTokenId, uint256 toTokenId, uint256 fee)",
+      ]),
+    },
+  },
+  [ChainId.Polygon]: {
+    lute: {
+      address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+      abi: new Interface([
+        "function totalSupply() external view returns (uint256)",
+        "function tokenURI(uint256 tokenId) external view returns (string memory)",
+        "function setApprovalForAll(address operator, bool approved) external",
+        "function isApprovedForAll(address owner, address operator) external view returns (bool)",
+        "event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId)",
+        "event ApprovalForAll(address indexed owner, address indexed operator, bool approved)",
+        "event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)",
+      ]),
+    },
+    flute: {
+      address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+      abi: new Interface([
+        "function totalSupply() external view returns (uint256)",
+        "function tokenURI(uint256 tokenId) external view returns (string memory)",
+        "function setApprovalForAll(address operator, bool approved) external",
+        "function isApprovedForAll(address owner, address operator) external view returns (bool)",
+        "event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId)",
+        "event ApprovalForAll(address indexed owner, address indexed operator, bool approved)",
+        "event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)",
+      ]),
+    },
+    luteDrop: {
+      address: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
+      abi: new Interface([
+        "function latestDrop() external view returns (uint256)",
+        "function drops(uint256 dropId) external view returns (tuple(uint256 fee, uint256 craftableSupply, uint256 craftedSupply, uint256 craftsPerAddress))",
+        "function crafts(address account, uint256 dropid) external view returns (uint256)",
+        "function craft(uint8 item, uint256 dropid) external payable",
+        "event Craft(address indexed to, uint8 indexed item, uint256 fee)",
+      ]),
+    },
+    lutiswap: {
+      address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+      abi: new Interface([
+        "function nextLute() external view returns (uint256)",
+        "function nextFlute() external view returns (uint256)",
+        "function latestSwapPrice() external view returns (uint256, uint256)",
+        "function swapExactLuteForFlute(uint256 tokenId) external payable",
+        "function swapExactFluteForLute(uint256 tokenId) external payable",
+        "event Swap(address indexed user, address indexed from, address indexed to, uint256 fromTokenId, uint256 toTokenId, uint256 fee)",
+      ]),
+    },
+  },
   [ChainId.Rinkeby]: {
     lute: {
       address: "0x662A31660f430eB5F93d28Af484045a196653e6d",
--- app/lutedrop/src/config/dapp.ts
+++ app/lutedrop/src/config/dapp.ts
@@ -23,6 +23,7 @@ const config: Config = {
   readOnlyUrls: {
     [ChainId.Hardhat]: "http://localhost:8545",
     [ChainId.Rinkeby]: "https://eth-rinkeby.alchemyapi.io/v2/",
+    [ChainId.Mumbai]: "https://polygon-mumbai.g.alchemy.com/v2/",
   },
   multicallAddresses: {
     [ChainId.Hardhat]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
