ae175fce271d3eea637330eff85664e3bde330d9
Connor Mendenhall <cmendenhall@8thlight.com>
2022-02-07 23:31:48 -0500

    Add web3modal

--- app/lutedrop/src/components/Connect.tsx
+++ app/lutedrop/src/components/Connect.tsx
@@ -1,21 +1,75 @@
-import { shortenAddress, useEthers } from "@usedapp/core";
-import { useCallback } from "react";
+import { shortenAddress, useEthers, useLookupAddress } from "@usedapp/core";
+import { useCallback, useEffect, useState } from "react";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
 const Connect = () => {
-  const { activateBrowserWallet, account } = useEthers();
+  const { account, activate, deactivate } = useEthers();
+  const ens = useLookupAddress();
+  const [activateError, setActivateError] = useState("");
+  const { error } = useEthers();
 
-  const activateWallet = useCallback(() => {
-    activateBrowserWallet();
-  }, [activateBrowserWallet]);
+  useEffect(() => {
+    if (error) {
+      setActivateError(error.message);
+    }
+  }, [error]);
+
+  const activateProvider = async () => {
+    const providerOptions = {
+      injected: {
+        display: {
+          name: "Metamask",
+          description: "Connect with the provider in your browser",
+        },
+        package: null,
+      },
+      walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+          bridge: "https://bridge.walletconnect.org",
+          infuraId: "142c1667bd93462e97f9060710aa66ba",
+        },
+      },
+    };
+
+    const web3Modal = new Web3Modal({
+      providerOptions,
+    });
+    try {
+      const provider = await web3Modal.connect();
+      await activate(provider);
+      setActivateError("");
+    } catch (error: any) {
+      console.log(error);
+      setActivateError(error.message);
+    }
+  };
 
   return (
-    <div className="md:fixed md:top-12 md:right-12 md:mb-0 md:z-50 mb-8 text-center">
-      <button
-        className="font-body text-xl px-4 py-2 bg-yellow-200 text-gray-800 hover:bg-yellow-400 rounded-md shadow"
-        onClick={activateWallet}
-      >
-        {account ? shortenAddress(account) : "Connect"}
-      </button>
+    <div>
+      <div className="md:fixed md:top-12 md:right-12 md:mb-0 md:z-50 mb-8 text-center">
+        {account ? (
+          <>
+            <span className="font-body text-xl px-4 py-2 bg-yellow-50 text-gray-800 hover:bg-yellow-400 rounded-md shadow mr-4">
+              {ens ?? shortenAddress(account)}
+            </span>
+            <button
+              className="font-body text-xl px-4 py-2 bg-yellow-200 text-gray-800 hover:bg-yellow-400 rounded-md shadow"
+              onClick={() => deactivate()}
+            >
+              Disconnect
+            </button>
+          </>
+        ) : (
+          <button
+            className="font-body text-xl px-4 py-2 bg-yellow-200 text-gray-800 hover:bg-yellow-400 rounded-md shadow"
+            onClick={activateProvider}
+          >
+            Connect
+          </button>
+        )}
+      </div>
     </div>
   );
 };
